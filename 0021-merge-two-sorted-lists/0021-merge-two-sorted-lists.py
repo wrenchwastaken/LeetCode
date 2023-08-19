dd3d386f5ee36800c36969f4b1bfdef922b150a6
Soyam Das <132677385+wrenchwastaken@users.noreply.github.com>
2023-08-19 14:44:46 +0530

    Time: 44 ms (76.7%), Space: 16.3 MB (50.45%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
@@ -9,18 +9,15 @@ class Solution:
         pointer = temp
         while list1 and list2:
             if list1.val<list2.val:
-                pointer.next=list1
-                list1=list1.next
+                pointer.next = list1
+                list1 = list1.next
             else:
                 pointer.next = list2
                 list2 = list2.next
-            pointer = pointer.next
+            pointer=pointer.next
         if list1:
             pointer.next=list1
         else:
-            pointer.next = list2
-        return temp.next
-
-    
-    
+            pointer.next=list2
+        return temp.next 
         
