1cb0ef421e48d945194d3fa977dd435744770222
Soyam Das <132677385+wrenchwastaken@users.noreply.github.com>
2023-08-19 17:12:56 +0530

    Time: 61 ms (90.68%), Space: 16.4 MB (64.94%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -8,14 +8,16 @@ class Solution:
         temp = ListNode()
         pointer = temp
         carry = 0
-        while l1 != None or l2 != None or carry != 0:
-            l1Val = l1.val if l1 else 0
-            l2Val = l2.val if l2 else 0
-            sum = l1Val + l2Val + carry
+        while l1!=None or l2!=None or carry!=0:
+            val1=l1.val if l1!=None else 0
+            val2=l2.val if l2!=None else 0
+            sum = val1 + val2 + carry
+            digit = sum%10
             carry = sum//10
-            newNode = ListNode(sum%10)
-            pointer.next = newNode
-            pointer = newNode
-            l1 = l1.next if l1 else None
-            l2 = l2.next if l2 else None
+            pointer.next=ListNode(digit)
+            pointer = pointer.next
+            l1=l1.next if l1!=None else None
+            l2=l2.next if l2!=None else None
         return temp.next
+    
+        
