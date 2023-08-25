fa90fd60100678c33e13029594889adcc9bcebe1
Soyam Das <132677385+wrenchwastaken@users.noreply.github.com>
2023-08-25 16:13:47 +0530

    Time: 39 ms (74.17%), Space: 16.4 MB (11.79%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -0,0 +1,11 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        pairs = {"(":")", "{":"}","[":"]"}
+        for i in s:
+            if i in pairs:
+                stack.append(i)
+            elif len(stack) == 0 or i!=pairs[stack.pop()]:
+                return False
+        return len(stack) == 0
+        
