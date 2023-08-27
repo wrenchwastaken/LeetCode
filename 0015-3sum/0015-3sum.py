dae498a0eed4e1f7331d940a3a7284d144e57167
Soyam Das <132677385+wrenchwastaken@users.noreply.github.com>
2023-08-27 22:15:55 +0530

    Time: 1052 ms (75.49%), Space: 20.3 MB (94.05%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,32 +1,26 @@
-from collections import defaultdict
-
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        negative = defaultdict(int)
-        positive = defaultdict(int)
-        zeros = 0
-        for num in nums:
-            if num < 0:
-                negative[num] += 1
-            elif num > 0:
-                positive[num] += 1
-            else:
-                zeros += 1
-        
+        nums.sort()
         result = []
-        if zeros:
-            for n in negative:
-                if -n in positive:
-                    result.append((0, n, -n))       
-            if zeros > 2:
-                result.append((0,0,0))
-
-        for set1, set2 in ((negative, positive), (positive, negative)):
-            set1Items = list(set1.items())
-            for i, (j, k) in enumerate(set1Items):
-                for j2, k2 in set1Items[i:]:
-                    if j != j2 or (j == j2 and k > 1):
-                        if -j-j2 in set2:
-                            result.append((j, j2, -j-j2))
+        
+        for i in range(len(nums)-2):
+            if i > 0 and nums[i] == nums[i-1]:
+                continue
+            left, right = i+1, len(nums)-1
+            while left < right:
+                sum = nums[i] + nums[left] + nums[right]
+                if sum == 0:
+                    result.append([nums[i], nums[left], nums[right]])
+                    left += 1
+                    right -= 1
+                    while left < right and nums[left] == nums[left-1]:
+                        left += 1
+                    while left < right and nums[right] == nums[right+1]:
+                        right -= 1
+                elif sum < 0:
+                    left += 1
+                else:
+                    right -= 1
+                    
         return result
         
