1650338646558455594b0c8a5894847b108f3cb1
Soyam Das <132677385+wrenchwastaken@users.noreply.github.com>
2023-08-31 21:10:06 +0530

    Time: 81 ms (5.5%), Space: 16.5 MB (7.36%) - LeetHub

---
+++ 0409-longest-palindrome/0409-longest-palindrome.py
@@ -0,0 +1,11 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> int:
+        ss = set()
+        for i in s:
+            if i not in ss:
+                ss.add(i)
+                print(ss)
+            else:
+                ss.remove(i)
+                print(ss)
+        return len(s)-len(ss) + 1 if len(ss)!=0 else len(s)
