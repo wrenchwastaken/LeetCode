c486e26698a0ba9ee0891b59bfa6d1bcbfa96cd5
Soyam Das <132677385+wrenchwastaken@users.noreply.github.com>
2023-09-05 21:31:52 +0530

    Time: 57 ms (51.93%), Space: 16.5 MB (15.1%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.py
+++ 0012-integer-to-roman/0012-integer-to-roman.py
@@ -1,8 +1,23 @@
 class Solution:
     def intToRoman(self, num: int) -> str:
-        M=["","M","MM","MMM"]
-        C=["","C","CC","CCC","CD","D","DC","DCC","DCCC","CM"]
-        X=["","X","XX","XXX","XL","L","LX","LXX","LXXX","XC"]
-        I=["","I","II","III","IV","V","VI","VII","VIII","IX"]
-        return M[num//1000] + C[num%1000//100] + X[num%1000%100//10] + I[num%1000%100%10]
+        # Creating Dictionary for Lookup
+        num_map = {
+            1: "I",
+            5: "V",    4: "IV",
+            10: "X",   9: "IX",
+            50: "L",   40: "XL",
+            100: "C",  90: "XC",
+            500: "D",  400: "CD",
+            1000: "M", 900: "CM",
+        }
         
+        # Result Variable
+        r = ''
+        
+        
+        for n in [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]:
+            # If n in list then add the roman value to result variable
+            while n <= num:
+                r += num_map[n]
+                num-=n
+        return r
