7cb270de4aee075d77bdbc99cebfc0227598aec7
Soyam Das <132677385+wrenchwastaken@users.noreply.github.com>
2023-09-11 23:46:37 +0530

    Time: 113 ms (78.77%), Space: 16.2 MB (89%) - LeetHub

--- 0264-ugly-number-ii/0264-ugly-number-ii.py
+++ 0264-ugly-number-ii/0264-ugly-number-ii.py
@@ -1,8 +1,15 @@
 class Solution:
     def nthUglyNumber(self, n: int) -> int:
-    	N, m, S = [1], 1, set()
-    	for _ in range(n):
-    		while m in S: m = heapq.heappop(N)
-    		S.add(m)
-    		for i in [2,3,5]: heapq.heappush(N,i*m)
-    	return m
+        ans =[1]
+        i,j,k = 0,0,0
+        while len(ans)<n:
+            a,b,c = ans[i]*2,ans[j]*3,ans[k]*5
+            min_element = min(a,b,c)
+            ans.append(min_element)
+            if min_element == a:
+                i+=1
+            if min_element == b:
+                j+=1
+            if min_element ==c:
+                k+=1
+        return ans[n-1]
