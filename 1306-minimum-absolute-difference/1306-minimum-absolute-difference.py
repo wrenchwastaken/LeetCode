7358a68cca8036a45f3754a354d128ae1e8b0536
Soyam Das <132677385+wrenchwastaken@users.noreply.github.com>
2023-09-13 14:46:40 +0530

    Time: 296 ms (56.8%), Space: 30.8 MB (99.11%) - LeetHub

--- 1306-minimum-absolute-difference/1306-minimum-absolute-difference.py
+++ 1306-minimum-absolute-difference/1306-minimum-absolute-difference.py
@@ -1,5 +1,25 @@
 class Solution:
-    def minimumAbsDifference(self, arr: List[int]) -> List[List[int]]:
+    def minimumAbsDifference(self, arr):
+        # Step 1: Sort the input array
         arr.sort()
-        m = min(arr[i+1]-arr[i] for i in range(len(arr)-1))
-        return [[arr[i],arr[i+1]]for i in range(len(arr)-1) if arr[i+1]-arr[i]==m]
+        
+        # Step 2: Initialize variables to keep track of the minimum absolute difference
+        min_diff = float('inf')
+        result = []
+        
+        # Step 3: Iterate through the sorted array and find the minimum difference
+        for i in range(len(arr) - 1):
+            diff = arr[i+1] - arr[i]
+            
+            # Step 4: If the current difference is smaller than the minimum, update the minimum
+            if diff < min_diff:
+                min_diff = diff
+                result = [[arr[i], arr[i+1]]]
+            
+            # Step 5: If the current difference is equal to the minimum, add it to the result
+            elif diff == min_diff:
+                result.append([arr[i], arr[i+1]])
+        
+        # Step 6: Return the result containing pairs with the minimum absolute difference
+        return result
+
