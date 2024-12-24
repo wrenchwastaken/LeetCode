e176e96f6808c07365a50e25dd053f9e128f4f3e
Soyam Das <132677385+wrenchwastaken@users.noreply.github.com>
2024-12-25 01:07:41 +0530

    Time: 0 ms (100%), Space: 28.6 MB (23%) - LeetHub

---
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<int> nextGreaterElements(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> ans(n);
+        stack<int> st;
+        for(int i = 2*n-1;i>=0;i--){
+            //actual index
+            int idx = i % n;
+            int curr = nums[idx];
+            //maintaining the monotonic stack
+            while(!st.empty() && st.top()<=curr){
+                st.pop();
+            }
+            //check if i is in bound before circle
+            if(i<n){
+                if(st.empty()){
+                    ans[i] = -1;
+                }
+                else{
+                    ans[i] = st.top();
+                }
+            }
+            st.push(curr);
+        }
+        return ans;
+    }
+};
