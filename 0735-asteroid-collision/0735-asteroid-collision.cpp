1f25b0058ee1fc3efb4e2a23ac360b84420ee68b
Soyam Das <132677385+wrenchwastaken@users.noreply.github.com>
2025-03-28 23:01:45 +0530

    Time: 0 ms (100%), Space: 21.6 MB (80.13%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.cpp
+++ 0735-asteroid-collision/0735-asteroid-collision.cpp
@@ -4,18 +4,22 @@ public:
         int n = asteroids.size();
         vector<int> st;
         for(int i=0;i<n;i++){
-            //putting asteroids into stack
+            //putting asteroids into stack if moving right
             if(asteroids[i]>0){
                 st.push_back(asteroids[i]);
             }
+            //if moving left
             else{
                 //sorting out negatives
+                //maintain stack, if smaller then gets destroyed
                 while(!st.empty() && st.back() > 0 && st.back() < abs(asteroids[i])){
                     st.pop_back();
                 }
+                //if same then gets destroyed
                 if(!st.empty() && st.back() == abs(asteroids[i])){
                     st.pop_back();
                 }
+                //if stack is empty or in same direction
                 else if(st.empty() || st.back() < 0){
                     st.push_back(asteroids[i]);
                 }
