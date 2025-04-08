05dc74ccee23ef1173514502375edb36a8ef524f
Soyam Das <132677385+wrenchwastaken@users.noreply.github.com>
2025-04-08 23:18:56 +0530

    Time: 6 ms (40.29%), Space: 15.6 MB (36.32%) - LeetHub

--- 0893-all-nodes-distance-k-in-binary-tree/0893-all-nodes-distance-k-in-binary-tree.cpp
+++ 0893-all-nodes-distance-k-in-binary-tree/0893-all-nodes-distance-k-in-binary-tree.cpp
@@ -9,50 +9,50 @@
  */
 class Solution {
 public:
-    void markParents(TreeNode* root, unordered_map<TreeNode*,TreeNode*> &parent, TreeNode* target){
-        queue<TreeNode*> q;
+    void markParents(TreeNode* root, unordered_map<TreeNode*, TreeNode*> &parents){
+        queue<TreeNode*>q;
         q.push(root);
         while(!q.empty()){
             TreeNode* node = q.front();
             q.pop();
-            if(node->left){
-                parent[node->left] = node;
+            if(node ->left != nullptr){
                 q.push(node->left);
-            }
-            if(node->right){
-                parent[node->right] = node;
+                parents[node->left] = node;
+            } 
+            if(node->right != nullptr){
                 q.push(node->right);
+                parents[node->right] = node;
             }
         }
     }
     vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
-        unordered_map<TreeNode*, TreeNode*> parent;
-        markParents(root,parent,target);
-        unordered_map<TreeNode*,bool> visited;
-        queue<TreeNode*>q;
+        unordered_map<TreeNode*,TreeNode*> parents;
+        markParents(root,parents);
+        unordered_map<TreeNode*, bool> visited;
+        queue<TreeNode*> q;
         q.push(target);
         visited[target] = true;
-        int level = 0;
+        int lvl = 0;
         while(!q.empty()){
             int n = q.size();
-            if(level == k){
+            if(lvl == k){
                 break;
             }
-            level++;
+            lvl++;
             for(int i=0;i<n;i++){
                 TreeNode* node = q.front();
                 q.pop();
-                if(node->left && !visited[node->left]){
+                if(node->left != nullptr && !visited[node->left]){
                     q.push(node->left);
                     visited[node->left] = true;
                 }
-                if(node->right && !visited[node->right]){
+                if(node->right != nullptr && !visited[node->right]){
                     q.push(node->right);
                     visited[node->right] = true;
                 }
-                if(parent[node] && !visited[parent[node]]){
-                    q.push(parent[node]);
-                    visited[parent[node]] = true;
+                if(parents[node] && !visited[parents[node]]){
+                    q.push(parents[node]);
+                    visited[parents[node]] = true;
                 }
             }
         }
@@ -61,7 +61,7 @@ public:
             TreeNode* node = q.front();
             q.pop();
             ans.push_back(node->val);
-        } 
+        }
         return ans;
     }
 };
